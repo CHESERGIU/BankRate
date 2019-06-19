9b708edd7be3b5235b54d3bf2a03e3ebce64f4bd
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-19 07:34:51 +0300

    test CalculateRateForThirdMonthForATenMonthCredit

--- BankRate/BankRate.Tests/Program.Tests.cs
+++ BankRate/BankRate.Tests/Program.Tests.cs
@@ -18,5 +18,12 @@ namespace BankRate.Tests
             decimal rate = Program.CalculateBankRate(200, 2, 12, 2);
             Assert.Equal(101, rate);
         }
+
+        [Fact]
+        public void CalculateRateForThirdMonthForATenMonthCredit()
+        {
+            decimal rate = Program.CalculateBankRate(2000, 10, 12, 3);
+            Assert.Equal(216, rate);
+        }
     }
 }
